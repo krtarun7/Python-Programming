759510f2d7f17f8dc6514418441269dd99c170df
krtarun7 <tarun7011kumar@gmail.com>
2026-05-25 18:30:45 +0530

    dictionary

---
+++ Variable/dictvalue.py
@@ -0,0 +1,8 @@
+marks={}
+x=int(input("Enter the number of students: "))
+marks.update({"phy":x})
+x=int(input("Enter the number of students: "))
+marks.update({"chem":x})
+x=int(input("Enter the number of students: "))
+marks.update({"maths":x})
+print(marks)
