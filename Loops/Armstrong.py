da805052fe1bcc8659d8a824f9452a9477c7881f
krtarun7 <tarun7011kumar@gmail.com>
2026-05-28 20:22:15 +0530

    Armstrong no

---
+++ Loops/Armstrong.py
@@ -0,0 +1,14 @@
+# Armstrong Number Check
+num = 153
+sum = 0
+temp = num
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+
+if num == sum:
+    print(num, "is an Armstrong Number")
+else:
+    print(num, "is not an Armstrong Number")
